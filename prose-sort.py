0b5225c044326da324d959c0f313a96a50e12055
chaines96 <haineschris777@gmail.com>
2025-08-28 10:18:47 -0600

    Created a prototype for a tkinter window.

--- prose-sort.py
+++ prose-sort.py
@@ -1,15 +1,15 @@
-import torch, numpy, pandas, sqlite3
+import torch, numpy, pandas, sqlite3, tkinter
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
 
-data_len = len(pd.read_csv("data.csv").iloc[0]) #The amount of rows in the CSV file, representing the number of entries of the training data.
+data_len = len(pandas.read_csv("data.csv").iloc[0]) #The amount of rows in the CSV file, representing the number of entries of the training data.
 batch_size = (data_len // 4) + 1 #The size of the batch of each epoch. Should sacle with the size of the data; I would consider hard coding this to 5.
 
 class ProseDataset(Dataset):
     def __init__(self, file_path, label_path, transform=None,target_transform=None):
-        pd.options.display.max_rows = 100 #Panda's default is 60
-        self.data = pd.read_csv(file_path)
-        self.labels = pd.read_csv(label_path)
+        pandas.options.display.max_rows = 100 #Panda's default is 60
+        self.data = pandas.read_csv(file_path)
+        self.labels = pandas.read_csv(label_path)
 
     def __len__(self):
         return len(self.data)
@@ -32,3 +32,8 @@ train_dataloader = DataLoader(training_data, batch_size=batch_size)
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+
+#This next block uses the tkinter library to create a GUI.
+root = Tk(screenName=None, baseName=None, className='Tk', useTk=1)
+window = m = tkinter.Tk()
+window.mainloop()
